0788f6fa68e2b6601b62786302a943c60597d119
cikezxy <cikezxy@163.com>
2018-10-24 21:29:37 +0800

    yaml config

--- src/main/java/com/cikezxy/springbootsandbox/config/ConfigTestController.java
+++ src/main/java/com/cikezxy/springbootsandbox/config/ConfigTestController.java
@@ -2,10 +2,13 @@ package com.cikezxy.springbootsandbox.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@PropertySource(value = "classpath:my.properties")
 public class ConfigTestController {
 
     @Autowired
@@ -14,13 +17,21 @@ public class ConfigTestController {
     @Value("${environments.url}")
     private String url;
 
-    @GetMapping(value="/ymlConfig")
-    public YmlEnvironmentConfig ymlEnvironmentConfig(){
+    @Value("${my.uuid}")
+    private String uuid;
+
+    @GetMapping(value = "/ymlConfig")
+    public YmlEnvironmentConfig ymlEnvironmentConfig() {
         return ymlEnvironmentConfig;
     }
 
-    @GetMapping(value="/environmentsUrl")
-    public String environmentsUrl(){
+    @GetMapping(value = "/environmentsUrl")
+    public String environmentsUrl() {
         return url;
+    }
+
+    @RequestMapping(value = "/uuid")
+    public String uuid() {
+        return uuid;
     }
 }
