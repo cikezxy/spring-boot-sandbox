12c5717c7ab5a84ee269914f68d343aa1d3f6f64
James.Zheng <James.Zheng@homecredit.cn>
2019-05-05 16:52:48 +0800

    add listener

--- src/main/java/com/cikezxy/springbootsandbox/SpringBootSandboxApplication.java
+++ src/main/java/com/cikezxy/springbootsandbox/SpringBootSandboxApplication.java
@@ -1,7 +1,6 @@
 package com.cikezxy.springbootsandbox;
 
 import com.cikezxy.springbootsandbox.listener.GenericListener;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
