3b642c5bb6b38ba6b728b1bc91ea734ee0294ee2
James.Zheng <James.Zheng@homecredit.cn>
2019-05-06 10:08:40 +0800

    external configuration

---
+++ src/main/java/com/cikezxy/springbootsandbox/config/ExternalConfiguration.java
@@ -0,0 +1,46 @@
+package com.cikezxy.springbootsandbox.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import java.util.UUID;
+
+@PropertySource(value = "classpath:my.properties")
+@Component
+public class ExternalConfiguration {
+
+    private static Logger log = LoggerFactory.getLogger(ExternalConfiguration.class);
+
+    @Value("${my.secret.password}")
+    private String password;
+
+    @Value("${my.secret.intValue}")
+    private Integer intValue;
+
+    @Value("${my.secret.intValueRange}")
+    private Integer intValueRange;
+
+    @Value("${my.secret.longValue}")
+    private Long longValue;
+
+    @Value("${my.secret.longValueRange}")
+    private Long longValueRange;
+
+    @Value("${my.secret.uuid}")
+    private UUID uuid;
+
+
+    @PostConstruct
+    private void init() {
+        log.info("Configure Random Property Values using Spring Boot");
+        log.info("password: " + password);
+        log.info("intValue: " + intValue);
+        log.info("intValueRange: " + intValueRange);
+        log.info("longValue: " + longValue);
+        log.info("longValueRange: " + longValueRange);
+        log.info("uuid: " + uuid);
+    }
+}
